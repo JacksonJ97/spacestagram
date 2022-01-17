0143539532272ad9b9f8b2e05a3aea8b8b58b246
JacksonJ97 <jacksonjude97@gmail.com>
2022-01-17 07:26:33 -0500

    created basic header

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+
+// Styles
+const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  background-color: #ffffff;
+  border-bottom: 1px solid #dbdbdb;
+  height: 60px; /* Test */
+
+  .logo {
+    font-family: "Lobster Two", cursive;
+    font-size: 1.75rem;
+  }
+`;
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <Wrapper>
+      <div className="logo">Spacestagram</div>
+    </Wrapper>
+  );
 };
 
 export default Header;
