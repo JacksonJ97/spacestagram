84484b77a31bd5041ea5432e7421526f20e43fdd
JacksonJ97 <jacksonjude97@gmail.com>
2022-01-31 17:32:44 -0500

    fixed useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,23 +5,16 @@ import useLocalStorage from "./useLocalStorage";
 
 // Helpers
 import formatData from "../helpers/formatData";
-import getStartDate from "../helpers/getStartDate";
 
 const useFetch = (url, shouldFetch) => {
-  // const { param } = getStartDate(new Date());
-  // const param = "2022-01-27";
   const [data, setData] = useLocalStorage("data", []);
-  // const [date, setDate] = useLocalStorage("date", null);
   const [error, setError] = useState(null);
-  // const checkStrings = param.localeCompare(date);
 
   useEffect(() => {
     const getData = async () => {
       try {
         if (shouldFetch) {
           const response = await fetch(url);
-          // setDate(param);
-          console.log("Fetched");
           if (response.status >= 200 && response.status <= 299) {
             const fetchedData = await response.json();
             const formattedData = formatData(fetchedData);
