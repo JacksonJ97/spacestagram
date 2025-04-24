49cbc3e6303088c68bea171382dab031c4b262fa
Jackson <jacksonjude97@gmail.com>
2025-04-24 01:13:46 -0400

    login and signup form

---
+++ src/components/common/TextInput/index.tsx
@@ -0,0 +1,77 @@
+import {
+  FieldPath,
+  FieldValues,
+  useController,
+  UseControllerProps,
+} from "react-hook-form";
+import { cn } from "utils/functions";
+
+type OmittedHTMLInputProps = "name" | "type" | "disabled" | "defaultValue";
+
+type TextInputProps = {
+  label: string;
+  type?: "text" | "password";
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, OmittedHTMLInputProps>;
+
+export default function TextInput<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  type = "text",
+  label,
+  required,
+  className,
+  // ControllerProps
+  name,
+  rules,
+  control,
+  disabled,
+  defaultValue,
+  ...props
+}: TextInputProps & UseControllerProps<TFieldValues, TName>) {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({
+    name,
+    rules,
+    control,
+    disabled,
+    defaultValue,
+  });
+
+  const isRequired = required || !!rules?.required;
+
+  return (
+    <div className="flex w-full flex-col gap-1.5">
+      <label
+        htmlFor={name}
+        className="text-sm font-semibold text-(--text-color)"
+      >
+        {label}
+      </label>
+      <input
+        id={name}
+        type={type}
+        autoCorrect="off"
+        autoCapitalize="off"
+        aria-label={label}
+        aria-required={isRequired ? true : undefined}
+        aria-invalid={error ? true : undefined}
+        aria-errormessage={error ? `${name}-error` : undefined}
+        className={cn(
+          "h-9 rounded-xs border border-(--border-color) bg-(--secondary-background-color) p-2 text-sm text-(--text-color)",
+          error && "border-(--error-color)",
+          className,
+        )}
+        {...field}
+        {...props}
+      />
+      {error && error.message && (
+        <p id={`${name}-error`} className="text-sm text-(--error-color)">
+          {error.message}
+        </p>
+      )}
+    </div>
+  );
+}
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,21 +1,72 @@
+import { z } from "zod";
 import { NavLink } from "react-router";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import TextInput from "components/common/TextInput";
+
+const schema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+  fullName: z.string().min(1, "Full name is required"),
+  username: z.string().min(1, "Username is required"),
+});
 
 export default function Signup() {
+  const { control, handleSubmit } = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+      fullName: "",
+      username: "",
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("data", data);
+  });
+
   return (
     <main className="min-h-screen bg-(--background-color) px-4 py-8">
       <div className="mx-auto max-w-sm">
         <div className="rounded-xs border border-(--border-color) px-2 py-3">
-          <h1 className="font-lobster my-9 text-center text-5xl text-(--text-color)">
+          <h1 className="font-lobster my-9 text-center text-4xl text-(--text-color) min-sm:text-5xl">
             Spacestagram
           </h1>
 
-          <div className="mx-auto my-6 flex max-w-xs flex-col items-center gap-1.5">
-            <div className="w-full text-(--text-color)">Email Input</div>
-            <div className="w-full text-(--text-color)">Password Input</div>
-            <div className="w-full text-(--text-color)">Full Name Input</div>
-            <div className="w-full text-(--text-color)">Username Input</div>
-            <div className="w-full text-(--text-color)">Signup Button</div>
-          </div>
+          <form
+            onSubmit={onSubmit}
+            className="mx-auto my-6 flex max-w-2xs flex-col items-center gap-3"
+          >
+            <TextInput required name="email" label="Email" control={control} />
+            <TextInput
+              required
+              type="password"
+              name="password"
+              label="Password"
+              control={control}
+              autoComplete="new-password"
+            />
+            <TextInput
+              required
+              name="fullName"
+              label="Full Name"
+              control={control}
+              autoCapitalize="sentences"
+            />
+            <TextInput
+              required
+              name="username"
+              label="Username"
+              control={control}
+            />
+            <button
+              type="submit"
+              className="w-full cursor-pointer text-(--text-color)"
+            >
+              Signup Button
+            </button>
+          </form>
         </div>
 
         <div className="mt-3 rounded-xs border border-(--border-color) px-2 py-4">
