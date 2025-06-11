7163fb271beb7d3310749828288cfc49125ce9d7
Jackson <jacksonjude97@gmail.com>
2025-06-10 23:47:19 -0400

    remove github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/spacestagram/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
