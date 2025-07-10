8f399678a96f732747b4d92959f5c68f09b8f7fe
Jackson <jacksonjude97@gmail.com>
2025-07-10 17:57:22 -0400

    added foreign key relations and updated schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,5 +1,7 @@
+import { relations } from "drizzle-orm";
 import {
   date,
+  unique,
   varchar,
   integer,
   timestamp,
@@ -20,17 +22,45 @@ export const users = pgTable("users", {
 });
 
 export const posts = pgTable("posts", {
-  date: date().primaryKey(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  date: date().unique().notNull(),
   title: varchar({ length: 150 }).notNull(),
   url: varchar({ length: 512 }).notNull(),
 });
 
-export const likedPosts = pgTable("liked_posts", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  // userId,
-  // postDate,
-  likedAt: timestamp({ withTimezone: true })
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
-});
+export const likedPosts = pgTable(
+  "liked_posts",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer()
+      .references(() => users.id, { onDelete: "cascade" })
+      .notNull(),
+    postId: integer()
+      .references(() => posts.id, { onDelete: "cascade" })
+      .notNull(),
+    likedAt: timestamp({ withTimezone: true })
+      .defaultNow()
+      .$onUpdate(() => new Date())
+      .notNull(),
+  },
+  (table) => [{ uniqueUserPost: unique().on(table.userId, table.postId) }]
+);
+
+export const usersRelations = relations(users, ({ many }) => ({
+  likedPosts: many(likedPosts),
+}));
+
+export const postsRelations = relations(posts, ({ many }) => ({
+  likedPosts: many(likedPosts),
+}));
+
+export const likedPostsRelations = relations(likedPosts, ({ one }) => ({
+  user: one(users, {
+    fields: [likedPosts.userId],
+    references: [users.id],
+  }),
+  post: one(posts, {
+    fields: [likedPosts.postId],
+    references: [posts.id],
+  }),
+}));
