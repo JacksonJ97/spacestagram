bec90131e3df1e3e486909f09e7ac68d50abd268
Jackson <jacksonjude97@gmail.com>
2025-07-13 03:06:29 -0400

    fixed redirect bug

--- client/src/data/user/hooks.ts
+++ client/src/data/user/hooks.ts
@@ -1,10 +1,11 @@
 import { toast } from "sonner";
-import { redirect } from "react-router";
+import { useNavigate } from "react-router";
 import { useMutation } from "@tanstack/react-query";
 import { api, getErrorMessage } from "utils/functions";
 import type { User, CreateUserInput } from "data/user/types";
 
 export function useCreateUser() {
+  const navigate = useNavigate();
   const createUser = async (user: CreateUserInput) => {
     const data = await api
       .post<User>("/api/users", user)
@@ -15,7 +16,7 @@ export function useCreateUser() {
   return useMutation({
     mutationFn: createUser,
     onSuccess: () => {
-      redirect("/");
+      navigate("/");
     },
     onError: (error) => {
       const message = getErrorMessage(error);
