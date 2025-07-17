35f7efee9e1da253be1700f1acb195ef85aab0ee
Jackson <jacksonjude97@gmail.com>
2025-07-17 18:13:07 -0400

    migrations

---
+++ server/drizzle/0001_keen_bedlam.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "sessions" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "sessions_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer NOT NULL,
+	"is_valid" boolean DEFAULT true,
+	"created_at" timestamp DEFAULT now(),
+	"expires_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
