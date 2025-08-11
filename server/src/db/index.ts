5f493162a1aaa05717a51344939acd222f4900b2
Jackson <jacksonjude97@gmail.com>
2025-08-11 04:35:54 -0400

    updated drizzle config

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,11 +1,9 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "db/schema";
 import { DATABASE_URL } from "constants/env";
 
-const db = drizzle({
-  schema,
-  casing: "snake_case",
-  connection: DATABASE_URL,
-});
+const sql = neon(DATABASE_URL);
+const db = drizzle({ schema, casing: "snake_case", client: sql });
 
 export default db;
