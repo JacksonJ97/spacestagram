f74a987b1d251c5e3cbb9b2872ca58603e7ae604
Jackson <jacksonjude97@gmail.com>
2025-08-12 02:23:24 -0400

    trying again

--- server/api/[...all].ts
+++ server/api/[...all].ts
@@ -1,4 +1,18 @@
-import "tsconfig-paths/register";
+// import path from "path";
+// import { register } from "tsconfig-paths";
+
+// register({
+//   baseUrl: path.join(__dirname, ".."),
+//   paths: {
+//     "constants/*": ["src/constants/*"],
+//     "controllers/*": ["src/controllers/*"],
+//     "db/*": ["src/db/*"],
+//     "middlewares/*": ["src/middlewares/*"],
+//     "routes/*": ["src/routes/*"],
+//     "utils/*": ["src/utils/*"],
+//   },
+// });
+
 import app from "../src/app";
 
 export default app;
